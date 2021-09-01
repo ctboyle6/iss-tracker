62e0687736dbad807149681fecda09021c376411
Chris Boyle <cthboyle6@gmail.com>
2021-09-01 15:25:04 -0400

    useEffect to pull coordinates on mount into state, map focused on state coordinates

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import getISS from './getISS.js';
+// import getISS from './getISS.js';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
-  const getCoords = async () => {
-    const response = await getISS();
-    console.log(response);
-  }
+  const [coords, setCoords] = useState({});
+  
+  useEffect(() => {
+    const getISS = async () => {
+      const url = 'https://api.wheretheiss.at/v1/satellites/25544'
+  
+      const response = await fetch(url);
+      const satelliteData = await response.json();
+  
+      // document.getElementById('latitude').textContent = satelliteData.latitude;
+      // document.getElementById('longitude').textContent = satelliteData.longitude;
 
-  getCoords();
+      setCoords(satelliteData);
+    };
+    
+    getISS();
+  }, [])
+
+  console.log(coords)
+
+  const showMap = () => {
+    if (coords.latitude) {
+      return (
+        <MapContainer center={[coords.latitude, coords.longitude]} zoom={2} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[coords.latitude, coords.longitude]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+      </MapContainer>
+      )
+    }
+  }
 
   return (
     <div className="App">
       <h1>ISS Tracker</h1>
       <div>
-        <h3>Latitude: <span id="latitude"></span></h3>
-        <h3>Longitude: <span id="longitude"></span></h3>
+        <h3>Latitude: <span id="latitude"></span>{coords.latitude}</h3>
+        <h3>Longitude: <span id="longitude"></span>{coords.longitude}</h3>
       </div>
-
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <div>
+        {showMap()}
+      </div>
     </div>
   );
 }
